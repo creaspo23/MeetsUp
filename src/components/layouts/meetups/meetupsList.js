ffb921612e57c7b47f7c92288410b9fadb0b0515
creaspo <creaspo23@gmail.com>
2022-07-20 17:29:48 +0200

    :fire: fetch data from firebase

--- src/components/layouts/meetups/meetupsList.js
+++ src/components/layouts/meetups/meetupsList.js
@@ -1,17 +1,21 @@
 import MeetsUpItem from './meetupItems';
 import classes from './MeetupList.module.css';
-function MeetupList(probs) {
+function MeetupList(props) {
+  
     return (
-        <ul className={classes.list}>
-            {probs.meetups.map(meetups => <MeetsUpItem
-                key={meetups.id}
-                id={meetups.id}
-                image={meetups.image}
-                title={meetups.title}
-                address={meetups.address}
-                description={meetups.description} />)}
-        </ul>
+      <ul className={classes.list}>
+        {props.meetups.map((meetup) => (
+          <MeetsUpItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+            description={meetup.description}
+          />
+        ))}
+      </ul>
     );
-}
+  }
 
 export default MeetupList;
